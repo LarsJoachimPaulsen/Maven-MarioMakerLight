3f305b8a38572fe7ce04bb89e6cbe310786c8ab1
Tasaladden <vsdostuen@hotmail.com>
2020-04-01 17:45:32 +0200

    Lagd stagebuilder og stage

---
+++ src/main/java/Gruppe5/Data/StageBuilder.java
@@ -0,0 +1,16 @@
+package Gruppe5.Data;
+
+public class StageBuilder {
+
+    Stage stage = new Stage();
+
+    public StageBuilder stageWidth(int stageWidth){
+        stage.stageWidth = stageWidth;
+        return this;
+    }
+
+    public StageBuilder stageHeight(int stageHeight){
+        stage.stageHeight = stageHeight;
+        return this;
+    }
+}
