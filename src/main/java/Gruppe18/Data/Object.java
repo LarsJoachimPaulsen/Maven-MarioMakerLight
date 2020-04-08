e3282d8a3690654fd0358a612889a26712f80831
CrispChamping <chisml>
2020-04-08 15:56:11 +0200

    object

--- src/main/java/Gruppe18/Data/Object.java
+++ src/main/java/Gruppe18/Data/Object.java
@@ -5,4 +5,9 @@ public class Object {
     //3 objects: square, circle and triangle
     //se oop figurer eksempelet fra 2 semester for referanser
 
+    public void setMaterial() {
+        //set the objects material as either metal, glass or rubber
+    }
+
+
 }
